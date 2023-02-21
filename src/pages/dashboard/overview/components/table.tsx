3df97b33a84e53d7338984fa5a9bccc5340154cb
zhao xuan <478193833@qq.com>
2023-02-21 16:39:59 +0800

    fix 1

---
+++ src/pages/dashboard/overview/components/table.tsx
@@ -0,0 +1,27 @@
+import ProCard from "@ant-design/pro-card";
+import PlotsLine from "./plotsline";
+
+const TableView = (): JSX.Element => {
+  return (
+    <>
+      {/* <ProCard ghost colSpan={24} layout="center">
+        <ProCard ghost colSpan={20} style={{ padding: '24px' }}>
+          <div className="title_count text-left">
+            <span>历史<span style={{ fontWeight: 'bolder' }}>DAU</span></span>
+          </div>
+          <div
+            style={{
+              marginTop: '40px'
+            }}
+          >
+            <PlotsLine
+              data={dauhistory}
+            />
+          </div>
+        </ProCard>
+      </ProCard> */}
+    </>
+  );
+}
+
+export default TableView;
